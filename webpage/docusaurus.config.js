4f39b20591211728137c27df19a69e84ab6fef3e
CagriCatik <mccatik@gmail.com>
2025-01-06 20:21:28 +0100

    test deployment

--- webpage/docusaurus.config.js
+++ webpage/docusaurus.config.js
@@ -4,213 +4,212 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Automated and Connected Driving',
-  tagline: 'This Knowledgebase will challenge your creativity, broaden your technical skills, and inspire you to shape the future of mobility',
-  favicon: 'img/favicon.ico',
+  title: "Automated and Connected Driving",
+  tagline:
+    "This Knowledgebase will challenge your creativity, broaden your technical skills, and inspire you to shape the future of mobility",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://cagricatik.github.io', // Correct: Root domain without sub-path
+  url: "https://cagricatik.github.io", // Correct: Root domain without sub-path
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Autonomous-Connected-Driving/', // Correct: Sub-path with trailing slash
+  baseUrl: "/Autonomous-Connected-Driving/", // Correct: Sub-path with trailing slash
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cagricatik', // Usually your GitHub org/user name.
-  projectName: 'Autonomous-Connected-Driving', // Usually your repo name.
+  organizationName: "cagricatik", // Usually your GitHub org/user name.
+  projectName: "Autonomous-Connected-Driving", // Usually your repo name.
 
   // Enable GitHub Pages deployment
-  deploymentBranch: 'gh-pages', // Default is 'gh-pages'
+  deploymentBranch: "gh-pages", // Default is 'gh-pages'
   trailingSlash: false, // Optional: depends on your preference
 
   // Additional configurations can go here
   // e.g., themeConfig, presets, plugins, etc.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "ignore",
   //onBrokenMarkdownLinks: 'ignore',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Add remark-math and rehype-katex plugins
-          remarkPlugins: [require('remark-math')],
-          rehypePlugins: [require('rehype-katex')],
+          remarkPlugins: [require("remark-math")],
+          rehypePlugins: [require("rehype-katex")],
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       },
     ],
   ],
-  
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'ACD',
+        title: "ACD",
         /*logo: {
           alt: 'My Site Logo',
           src: 'img/logo.png',
         },*/
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'introSidebar',
-            position: 'left',
-            label: 'Introduction',
+            type: "docSidebar",
+            sidebarId: "introSidebar",
+            position: "left",
+            label: "Introduction",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'toolsSidebar',
-            position: 'left',
-            label: 'Tools',
+            type: "docSidebar",
+            sidebarId: "toolsSidebar",
+            position: "left",
+            label: "Tools",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'sensorSidebar',
-            position: 'left',
-            label: 'Sensor Data Processing',
+            type: "docSidebar",
+            sidebarId: "sensorSidebar",
+            position: "left",
+            label: "Sensor Data Processing",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'objectSidebar',
-            position: 'left',
-            label: 'Object Fusion and Tracking',
+            type: "docSidebar",
+            sidebarId: "objectSidebar",
+            position: "left",
+            label: "Object Fusion and Tracking",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'vehicleSidebar',
-            position: 'left',
-            label: 'Vehicle Guidance',
+            type: "docSidebar",
+            sidebarId: "vehicleSidebar",
+            position: "left",
+            label: "Vehicle Guidance",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'connectedSidebar',
-            position: 'left',
-            label: 'Connected Driving',
+            type: "docSidebar",
+            sidebarId: "connectedSidebar",
+            position: "left",
+            label: "Connected Driving",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'taskSidebar',
-            position: 'left',
-            label: 'Tasks',
-          },  
-          {
-            type: 'docSidebar',
-            sidebarId: 'cppSidebar',
-            position: 'right',
-            label: 'C++',
+            type: "docSidebar",
+            sidebarId: "taskSidebar",
+            position: "left",
+            label: "Tasks",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'pySidebar',
-            position: 'right',
-            label: 'Python',
+            type: "docSidebar",
+            sidebarId: "cppSidebar",
+            position: "right",
+            label: "C++",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'rosSidebar',
-            position: 'right',
-            label: 'ROS',
+            type: "docSidebar",
+            sidebarId: "pySidebar",
+            position: "right",
+            label: "Python",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'ros2Sidebar',
-            position: 'right',
-            label: 'ROS2',
+            type: "docSidebar",
+            sidebarId: "rosSidebar",
+            position: "right",
+            label: "ROS",
+          },
+          {
+            type: "docSidebar",
+            sidebarId: "ros2Sidebar",
+            position: "right",
+            label: "ROS2",
           },
         ],
-        
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'ACD',
+            title: "ACD",
             items: [
               {
-                label: 'Theory',
-                to: '/docs/theory',
+                label: "Theory",
+                to: "/docs/theory/content",
               },
             ],
           },
-          
+
           {
-            title: 'Coding',
+            title: "Coding",
             items: [
               {
-                label: 'C++',
-                to: '/docs/cpp',
+                label: "C++",
+                to: "/docs/cpp/content",
               },
               {
-                label: 'Python',
-                to: '/docs/python',
+                label: "Python",
+                to: "/docs/python/content",
+              },
+            ],
+          },
+
+          {
+            title: "Robot Operating System",
+            items: [
+              {
+                label: "ROS",
+                to: "/docs/ros/content",
+              },
+              {
+                label: "ROS2",
+                to: "/docs/ros2/content",
               },
             ],
           },
           {
-            title: 'Robot Operating System',
+            title: "More",
             items: [
               {
-                label: 'ROS',
-                to: '/docs/ros',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'ROS2',
-                to: '/docs/ros2',
+                label: "GitHub",
+                href: "https://github.com/CagriCatik/ACD",
               },
             ],
           },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/CagriCatik/ACD',
-              },
-            ],
-          },
-          
         ],
         copyright: `Copyright © ${new Date().getFullYear()} - Automated and Connected Driving.`,
       },
